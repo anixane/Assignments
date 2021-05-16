1ae822b902b1cbbe90eb0af8e5407705b9469cde
Animesh Kumar <animeshkumar@Animeshs-MacBook-Pro-2.local>
2021-05-16 10:03:21 +0530

    Added 2 pointer approach for rainwater trap problem

--- Stacks/RainwaterTrap.py
+++ Stacks/RainwaterTrap.py
@@ -1,5 +1,9 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        """
+        Approach: we just need to focus on water over a particular bar.
+        This boils down to the problem of knowing local maxima on both sides of the bar.
+        """
         #using prefix and postfix arrays for local maximas
         def getTrappedWater(heights):
             if not heights: return 0
@@ -8,10 +12,14 @@ class Solution:
             for height in heights:
                 maxx = max(maxx,height)
                 left.append(maxx)
+                
+            #re-initialize maxx variable for traversing list in reverse dxn
             maxx = float('-inf')
             for i in range(len(heights)-1,-1,-1):
                 maxx = max(maxx,heights[i])
                 right.append(maxx)
+            
+            #reversing for makng the order correct.
             right.reverse()
             
             #boundary buildings have no water over it, so we can skip those in loop for calculating ans
@@ -20,5 +28,27 @@ class Solution:
                 result+=(min(left[i],right[i])-heights[i])
             return result
         
-        return getTrappedWater(height)
+        def twoPointerApproach(heights):
+            if not heights: return 0
+            result = 0
+            left,right = 0,len(heights)-1
+            l_max,r_max = heights[left],heights[right]
+            while left<right:
+                if heights[left]<heights[right]:
+                    if heights[left]>l_max:
+                        #just update the left-max
+                        l_max = heights[left]
+                    else:
+                        result+=(l_max-heights[left])
+                    left+=1
+                else:
+                    if heights[right]>r_max:
+                        #update right max
+                        r_max = heights[right]
+                    else:
+                        result+=(r_max-heights[right])
+                    right-=1
+            return result
+        
+        return twoPointerApproach(height)
         
