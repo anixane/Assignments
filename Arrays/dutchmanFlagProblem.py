a810365fd97ba2b5cedf7961ab5d81eae9fcbf28
Animesh Kumar <animeshkumar@Animeshs-MacBook-Pro-2.local>
2021-05-23 08:27:12 +0530

    Added dutchman flag problem

---
+++ Arrays/dutchmanFlagProblem.py
@@ -0,0 +1,22 @@
+"""
+Problem: Sort an array of having elements 0,1,2
+"""
+
+def sortDutchanProblem( arr): 
+    lo = 0
+    hi = len(arr) - 1
+    mid = 0
+    while mid <= hi: 
+        if arr[mid] == 0: 
+            arr[lo], arr[mid] = arr[mid], arr[lo] 
+            lo = lo + 1
+            mid = mid + 1
+        elif arr[mid] == 1: 
+            mid = mid + 1
+        else: 
+            arr[mid], arr[hi] = arr[hi], arr[mid]  
+            hi = hi - 1
+    return arr 
+
+arr = [2,2,1,0,1,1,2,0,0]
+print(sortDutchanProblem(arr))
