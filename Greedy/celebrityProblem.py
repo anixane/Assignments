86604b83d5ece78965628b9f149a95f1a7abe64c
Animesh Kumar <pausethismoment@gmail.com>
2021-10-19 23:59:16 +0530

    Update celebrityProblem.py

--- Greedy/celebrityProblem.py
+++ Greedy/celebrityProblem.py
@@ -2,9 +2,8 @@
 Problem: https://practice.geeksforgeeks.org/problems/the-celebrity-problem/
 """
 
+#TC: O(n-square), SC: O(n)
 class Solution:
-    
-    #Function to find if there is a celebrity in the party or not.
     def celebrity(self, M, n):
         indegree, outdegree = [0 for _ in range(n)], [0 for _ in range(n)]
         for i in range(n):
@@ -17,3 +16,23 @@ class Solution:
             if outdegree[i]==0 and indegree[i]==n-1:
                 return i
         return -1
+
+################ OPTIMIZED CODE ##################################
+    
+    
+#TC: O(n), SC: O(1)  
+class Solution:
+    def celebrity(self, M, n):
+        """
+        Optimization: O(n)
+        """
+        celeb = 0
+        for i in range(n):
+            if M[celeb][i]==1:
+                celeb=i
+        #verify
+        for i in range(n):
+            if i!=celeb:
+                if M[celeb][i]==1 or M[i][celeb]==0:
+                    return -1
+        return celeb
